987a1822265484fbb9ec8184fd4e7a2d5e87a493
acer <acer@10.0.0.103>
2023-10-04 16:28:57 +0530

    first commit

--- project1/src/com/tns/inheritanceprog/SingleDemo.java
+++ project1/src/com/tns/inheritanceprog/SingleDemo.java
@@ -4,7 +4,8 @@ public class SingleDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Student st=new Student("vanaja",1234567,"penugonda",949140);
+		System.out.println(st);
 
 	}
 
