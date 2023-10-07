b6758834a1acb47c71bf90920aa9f8bcd24c58ed
acer <acer@10.0.0.104>
2023-10-07 14:51:28 +0530

    first commit

---
+++ project1/src/com/tns/finalpg/FinalDemo.java
@@ -0,0 +1,17 @@
+package com.tns.finalpg;
+
+public class FinalDemo {
+	int a=20;
+	void display() {
+		System.out.println("the value of a is"+a);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FinalDemo Fd=new FinalDemo();
+		Fd.display();
+		
+
+	}
+
+}
